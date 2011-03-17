a80a59bf41c0a1631c94057675f896f3780e9067
julien <julien.genestoux@gmail.com>
2011-03-17 11:39:59 -0700

    cleaned up the background page

--- controllers/strophe.superfeedr.js
+++ controllers/strophe.superfeedr.js
@@ -97,8 +97,6 @@ Strophe.addConnectionPlugin('superfeedr', {
             published: Strophe.getText(atom.getElementsByTagName("published")[0]),
             updated: Strophe.getText(atom.getElementsByTagName("updated")[0]),
             title: Strophe.getText(atom.getElementsByTagName("title")[0]),
-            summary: Strophe.getText(atom.getElementsByTagName("summary")[0]),
-            content: Strophe.getText(atom.getElementsByTagName("content")[0]),
             links: links,
             source: source
           });
