8d2da4bfa70bf1718ceff4051af040260bd808c9
julien <julien.genestoux@gmail.com>
2011-04-10 12:37:10 +0200

    adding read all or delete all

--- models/archive.js
+++ models/archive.js
@@ -1,4 +1,24 @@
 var Archive = Backbone.Collection.extend({
     storeName: "messages",
-    model: Message
+    model: Message,
+
+	mark_all_as_read: function() {
+		_.each(this, function(item, index) {
+			model = this.at(index);
+			if(model) {
+				model.set({"read": false});
+				model.save();
+			}
+		}, this)
+	},
+	
+	delete_all: function() {
+		_.each(this, function(item, index) {
+			model = this.at(index);
+			if(model) {
+				model.destroy();
+			}
+		}, this);
+	},
+	
 })
