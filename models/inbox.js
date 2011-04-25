184606e22bbe828578928b4eef70ab5288a7ef2a
julien <julien.genestoux@gmail.com>
2011-04-25 15:51:00 -0400

    improving notification that are now not sync anymore

--- models/inbox.js
+++ models/inbox.js
@@ -11,12 +11,14 @@ var Inbox = Backbone.Model.extend({
   addMessage: function(msg) {
 	// Adds the message if the message isn't yet present
 	var message = new Message({'id': msg.id})
+	var that = this;
 	message.fetch({
 		error: function() {
 			// The message was not found, so we just have to create one!
 			message.set(msg) // And now set the attributes.
 			message.collection = this.messages;
 			message.save();
+			that.trigger("messages:added", message.id)
 		},
 		success: function() {
 			// Success, we should return null, as this message was not added, because it already existed!
