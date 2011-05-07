b1fee641769c9e16b6142465e0049866998f69db
julien <julien.genestoux@gmail.com>
2011-05-07 14:06:06 +0200

    fixing multiple issues and bugs

--- controllers/run_plugins.js
+++ controllers/run_plugins.js
@@ -1,6 +1,6 @@
 // Runs all the plugins
 $.each(Plugins.all, function(index, plugin) {
-  chrome.extension.sendRequest({"settings": {"get" : ["plugins."+nameToId(plugin.name)]}}, function(response) {
+  chrome.extension.sendRequest({"settings": {"get" : ["plugin-"+nameToId(plugin.name)]}}, function(response) {
     if(response.value) {
       // This plugin is installed!
       if(plugin.onSubscriptionPage()) { // Are we on the plugin's page?
--- models/archive.js
+++ models/archive.js
@@ -3,16 +3,38 @@ var Archive = Backbone.Collection.extend({
 	database: msgboyDatabase,
     model: Message,
 
-	mark_all_as_read: function() {
-		_.each(this, function(item, index) {
-			model = this.at(index);
-			if(model) {
-				model.save({
-					read_at: new Date().getTime(),
-					unread_at: 0
-				});
-			}
-		}, this)
+	mark_all_as_read: function(opts) {
+		opts = typeof(opts) != 'undefined' ? opts : {};
+		s = opts.start || 0
+		this.fetch({
+			conditions: {unread_at: [s, s + 60000]},
+			success: function() {
+				console.log(this.length)
+				_.each(this.models, function(item, index) {
+					console.log(item)
+					if(item) {
+						item.save({
+							read_at: new Date().getTime(),
+							unread_at: 0
+						});
+					}
+				}, this)
+				this.mark_all_as_read({'start': s + 60000, 'done': opts.done})
+				console.log("DONE")
+			}.bind(this)
+		})
+		// 
+		// _.each(this, function(item, index) {
+		// 	model = this.at(index);
+		// 	if(model) {
+		// 		model.save({
+		// 			read_at: new Date().getTime(),
+		// 			unread_at: 0
+		// 		});
+		// 	}
+		// }, this)
+		// 
+		
 	},
 	
 	delete_all: function() {
