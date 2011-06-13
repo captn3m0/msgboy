2152bccf40293116b3b7ff71ea5a964750e2e430
julien <julien.genestoux@gmail.com>
2011-06-13 13:51:59 -0400

    adding the generic plugin which lets any site owner map their subscriptions with the msgboy's

---
+++ controllers/plugins/generic.js
@@ -0,0 +1,28 @@
+Plugins.register(new function() {
+	this.name = 'Generic Plugin which will listen for any page', 
+	
+	this.onSubscriptionPage = function() {
+	    return true;
+	},
+	
+	this.listSubscriptions = function(callback) {
+        callback([]); 
+    },
+	
+	this.hijack = function(follow, unfollow) {
+	    // Adds a listen event on all elements
+	    $(".msgboy-follow").click(function(element) {
+	        follow({
+				title: $(element.currentTarget).attr("data-msgboy-title"),
+				url: $(element.currentTarget).attr("data-msgboy-url")
+		    }, function() {
+		        // Done
+		    });
+    	    return false;
+	    })
+	},
+	
+	this.isUsing = function(callback) {
+		callback(true);
+	}
+});
