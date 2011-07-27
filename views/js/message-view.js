c432ad39bb5b839cea2b3dd157dd5ad5aeac988b
Eric Zanol <ezanol@gmail.com>
2011-07-27 00:39:13 -0700

    making progress on dashboard layout issues and notifications.

--- views/js/message-view.js
+++ views/js/message-view.js
@@ -79,20 +79,11 @@ var MessageView = Backbone.View.extend({
                         this.$("img").css("max-width", "100%");
                     }
                     
-                    // trying something here. -eric
-                    //this.$("h1").text(this.model.attributes.title).appendTo($(this.el));
+                    // show the source title.
                     this.$("h1").text(this.model.attributes.source.title).appendTo($(this.el));
-                    /*
-$(this.el).bind("mouseover", function() {
-                        this.$("h1").text(this.model.attributes.source.title).appendTo($(this.el));
-                    }.bind(this));
-                    $(this.el).bind("mouseout", function() {
-                        this.$("h1").text(this.model.attributes.title).appendTo($(this.el));
-                    }.bind(this));
-*/
                 }
             }.bind(this));
-        }.bind(this), 3000); // For now, let's wait for 2 seconds. It would be much much better if we had a callback that works when images have been loaded.
+        }.bind(this), 2000); // For now, let's wait for 2 seconds. It would be much much better if we had a callback that works when images have been loaded.
         
         // Adding the rest of the content.
         $("<p>").html(MsgboyHelper.cleaner.html(this.model.attributes.title)).appendTo($(this.el));
@@ -106,6 +97,7 @@ $(this.el).bind("mouseover", function() {
             sum += c.charCodeAt(0);
         });
         //$(this.el).addClass("color" + sum%7);
+        // using grayscale for the time being. pending new color palette. -&yet:eric
         $(this.el).css("background-color", "hsl(240,0%," + (sum%7)*10 + "%)");
         //$("<p>").html(MsgboyHelper.cleaner.html(sum%7)).appendTo($(this.el));
         this.trigger("rendered");
@@ -123,10 +115,16 @@ $(this.el).bind("mouseover", function() {
     
     // Message was voted down
     down: function() {
-        $(this.el).removeClass("brick-2");
-        $(this.el).removeClass("brick-3");
-        $(this.el).removeClass("brick-4");
-        $(this.el).addClass("brick-1");
+        if ($(this.el).hasClass("brick-2")) {
+            $(this.el).removeClass("brick-2");
+            $(this.el).addClass("brick-1");
+        } else if ($(this.el).hasClass("brick-3")) {
+            $(this.el).removeClass("brick-3");
+            $(this.el).addClass("brick-2");
+        } else if ($(this.el).hasClass("brick-4")) {
+            $(this.el).removeClass("brick-4");
+            $(this.el).addClass("brick-3");
+        }
         // this.trigger("change");
         this.model.vote_down(function(result) {
             if(result.unsubscribe) {
