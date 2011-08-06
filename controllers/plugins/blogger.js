8130c1ceb695cc41ffa3014438144021ef5e10c1
julien <julien.genestoux@gmail.com>
2011-08-06 10:22:02 +0200

    linting the blogger module

--- controllers/plugins/blogger.js
+++ controllers/plugins/blogger.js
@@ -1,10 +1,10 @@
 // Blogger
-Plugins.register(new function () {
+Plugins.register(function () {
 
-    this.name = 'Blogger', // Name for this plugin. The user will be asked which plugins he wants to use.
+    this.name = 'Blogger'; // Name for this plugin. The user will be asked which plugins he wants to use.
     this.onSubscriptionPage = function () {
-        return (window.location.host == "www.blogger.com" && window.location.pathname == '/navbar.g')
-    },
+        return (window.location.host == "www.blogger.com" && window.location.pathname == '/navbar.g');
+    };
 
     this.hijack = function (follow, unfollow) {
         $('a#b-follow-this').click(function (event) {
@@ -15,33 +15,33 @@ Plugins.register(new function () {
                 // Done
             });
         });
-    }
+    };
 
     this.isUsing = function (callback) {
         var that = this;
         $.get("http://www.blogger.com/home", function (data) {
-            menu = $(data).find("#global-info")
+            menu = $(data).find("#global-info");
             if (menu.length === 0) {
                 callback(false);
             } else {
                 callback(true);
             }
         });
-    },
+    };
 
     this.listSubscriptions = function (callback) {
         var subscriptions = [];
         $.get("http://www.blogger.com/manage-blogs-following.g", function (data) {
              var rex = /createSubscriptionInUi\(([\s\S]*?),[\s\S]*?,([\s\S]*?),[\s\S]*?,[\s\S]*?,[\s\S]*?,[\s\S]*?,[\s\S]*?\);/g;
              var match = rex.exec(data);
-             while (match != null) {
+             while (match) {
                  subscriptions.push({
                      url: match[2].replace(/"/g,'').trim() + "feeds/posts/default",
                      title: match[1].replace(/"/g,'').trim()
-                 })
-                 var match = rex.exec(data);
+                 });
+                 match = rex.exec(data);
              }
             callback(subscriptions);
         }.bind(this));
-    }
+    };
 });
