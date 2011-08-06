6dfcb852a41cd3770be3545316f1371ad2e6c75a
julien <julien.genestoux@gmail.com>
2011-08-06 11:27:18 +0200

    linting disqus.js

--- controllers/plugins/disqus.js
+++ controllers/plugins/disqus.js
@@ -1,29 +1,29 @@
-Plugins.register(new function() {
+Plugins.register(function() {
 
-	this.name = 'Disqus Comments',
+    this.name = 'Disqus Comments';
 
-	this.onSubscriptionPage = function() {
-		// This method returns true if the plugin needs to be applied on this page.
-		return (document.getElementById("disqus_thread") != null)
-	},
+    this.onSubscriptionPage = function() {
+        // This method returns true if the plugin needs to be applied on this page.
+        return (document.getElementById("disqus_thread"));
+    };
 
-	this.hijack = function(follow, unfollow) {
-		$("#dsq-post-button").live('click', function(event) {
-		    follow({
-		        url: $(".dsq-subscribe-rss a").attr("href"),
-		        title: document.title + " comments"
-		    }, function() {
-		        //Done
-		    });
-		});
-	},
-	
-	this.listSubscriptions = function(callback) {
+    this.hijack = function(follow, unfollow) {
+        $("#dsq-post-button").live('click', function(event) {
+            follow({
+                url: $(".dsq-subscribe-rss a").attr("href"),
+                title: document.title + " comments"
+            }, function() {
+                //Done
+            });
+        });
+    };
+
+    this.listSubscriptions = function(callback) {
         callback([]); // We're not able to list all subscriptions
-    },
-	
-	this.isUsing = function(callback) {
-		callback(false) // By default we won't show
-	}
-	
+    };
+
+    this.isUsing = function(callback) {
+        callback(false); // By default we won't show
+    };
+
 });
