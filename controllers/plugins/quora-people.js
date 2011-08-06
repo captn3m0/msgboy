3bfa0f9a0ebc2fe89d061aed3ea3792b228b6e9e
julien <julien.genestoux@gmail.com>
2011-08-06 11:20:54 +0200

    indenting quora people

--- controllers/plugins/quora-people.js
+++ controllers/plugins/quora-people.js
@@ -1,44 +1,44 @@
 // Typepad
-Plugins.register(new function() {
+Plugins.register(function() {
 
-	this.name = 'Quora People', 
+    this.name = 'Quora People';
 
-	this.onSubscriptionPage = function() {
-		return (window.location.host == "www.quora.com")
-	},
+    this.onSubscriptionPage = function() {
+        return (window.location.host == "www.quora.com");
+    };
 
-	this.hijack = function(follow, unfollow) {
-		$(".follow_button").not(".unfollow_button").not(".topic_follow").click(function(event) {
-			if($.trim($(event.target).html()) == "Follow Question") {
-				// Looks like Questions don't have RSS feeds just yet.
-			}
-			else {
-				// This is must a button on a user's page. Which we want to follo
-				follow({
-					title: document.title,
-					url: window.location.href + "/rss"
-				}, function() {
-				    // Done
-				});
-			}
-		});
-	},
+    this.hijack = function(follow, unfollow) {
+        $(".follow_button").not(".unfollow_button").not(".topic_follow").click(function(event) {
+            if($.trim($(event.target).html()) == "Follow Question") {
+                // Looks like Questions don't have RSS feeds just yet.
+            }
+            else {
+                // This is must a button on a user's page. Which we want to follo
+                follow({
+                    title: document.title,
+                    url: window.location.href + "/rss"
+                }, function() {
+                    // Done
+                });
+            }
+        });
+    };
 
-	this.listSubscriptions = function(callback) {
+    this.listSubscriptions = function(callback) {
         callback([]); // We're not able to list all subscriptions
-    },
+    };
 
-	this.isUsing = function(callback) {
-		var that = this;
-		req = $.get("http://www.quora.com/inbox", function(data) {
-			menu = $(data).find(".signup")
-			if(menu.length === 0) {
-				callback(true);
-			}
-			else {
-				callback(false);
-			}
-		});
-	}
+    this.isUsing = function(callback) {
+        var that = this;
+        req = $.get("http://www.quora.com/inbox", function(data) {
+            menu = $(data).find(".signup");
+            if(menu.length === 0) {
+                callback(true);
+            }
+            else {
+                callback(false);
+            }
+        });
+    };
 
 });
