a57020df4e5e2ceb1117725ee61fd2a0434227e9
julien <julien.genestoux@gmail.com>
2011-08-06 11:16:36 +0200

    linting the digg plugin

--- controllers/plugins/digg.js
+++ controllers/plugins/digg.js
@@ -1,54 +1,54 @@
 // Digg
-Plugins.register(new function() {
+Plugins.register(function () {
 
-	this.name = 'Digg', // Name for this plugin. The user will be asked which plugins he wants to use.
+    this.name = 'Digg'; // Name for this plugin. The user will be asked which plugins he wants to use.
 
-	this.onSubscriptionPage = function() {
-		// This method returns true if the plugin needs to be applied on this page.
-		return (window.location.host == "digg.com")
-	},
+    this.onSubscriptionPage = function() {
+        // This method returns true if the plugin needs to be applied on this page.
+        return (window.location.host == "digg.com");
+    };
 
-	this.hijack = function(follow, unfollow) {
-		// This methods hijacks the susbcription action on the specific website for this plugin.
-		$(".btn-follow").live('click', function(event) {
-			url = $(event.target).attr("href");
-			login =  url.split("/")[1]
-			action = url.split("/")[2]
-			switch(action) {
-				case "follow":
-				follow({
-				    url: "http://digg.com/" + login + ".rss",
-				    title: login + " on Digg"
-				}, function() {
-				    // Done
-    			});
-				break;
-				case "unfollow":
-				unfollow({
-				    url: "http://digg.com/" + login + ".rss",
-				    title: login + " on Digg"
-				}, function() {
-				    // Done
-				});
-				break;
-				default:
-			}
-		});  
-	},
+    this.hijack = function(follow, unfollow) {
+        // This methods hijacks the susbcription action on the specific website for this plugin.
+        $(".btn-follow").live('click', function(event) {
+            url = $(event.target).attr("href");
+            login =  url.split("/")[1];
+            action = url.split("/")[2];
+            switch(action) {
+                case "follow":
+                follow({
+                    url: "http://digg.com/" + login + ".rss",
+                    title: login + " on Digg"
+                }, function() {
+                    // Done
+                });
+                break;
+                case "unfollow":
+                unfollow({
+                    url: "http://digg.com/" + login + ".rss",
+                    title: login + " on Digg"
+                }, function() {
+                    // Done
+                });
+                break;
+                default:
+            }
+        });  
+    };
 
     this.listSubscriptions = function(callback) {
         callback([]); // We're not able to list all subscriptions
-    },
-	
-	this.isUsing = function(callback) {
-		var that = this;
-		$.get("http://digg.com/", function(data) {
-			if($(data).find(".current-user").length === 0) {
-				callback(false);
-			}
-			else {
-				callback(true);
-			}
-		});
-	}
+    };
+
+    this.isUsing = function(callback) {
+        var that = this;
+        $.get("http://digg.com/", function(data) {
+            if($(data).find(".current-user").length === 0) {
+                callback(false);
+            }
+            else {
+                callback(true);
+            }
+        });
+    };
 });
