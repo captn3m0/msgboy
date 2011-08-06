7816348a333216219cf25145cf3e1c2e54f693b7
julien <julien.genestoux@gmail.com>
2011-08-06 11:23:25 +0200

    linting typepad

--- controllers/plugins/typepad.js
+++ controllers/plugins/typepad.js
@@ -1,39 +1,39 @@
 // Typepad
-Plugins.register(new function() {
+Plugins.register(function() {
 
-	this.name = 'Typepad', // Name for this plugin. The user will be asked which plugins he wants to use.
+    this.name = 'Typepad'; // Name for this plugin. The user will be asked which plugins he wants to use.
 
-	this.onSubscriptionPage = function() {
-		return (window.location.host == "www.typepad.com" && window.location.pathname == '/services/toolbar')
-	},
+    this.onSubscriptionPage = function() {
+        return (window.location.host == "www.typepad.com" && window.location.pathname == '/services/toolbar');
+    };
 
-	this.hijack = function(follow, unfollow) {
-		$("#follow-display").click(function() {
-		    follow({
-		        title: $.trim($($("#unfollow-display a")[0]).html()) + " on Typepad",
-				href : $($("#unfollow-display a")[0]).attr("href") + "/activity/atom.xml"
-			}, function() {
-			    // Done
-			});
-			return false;
-		});
-	},
-	
-	this.listSubscriptions = function(callback) {
+    this.hijack = function(follow, unfollow) {
+        $("#follow-display").click(function() {
+            follow({
+                title: $.trim($($("#unfollow-display a")[0]).html()) + " on Typepad",
+                href : $($("#unfollow-display a")[0]).attr("href") + "/activity/atom.xml"
+            }, function() {
+                // Done
+            });
+            return false;
+        });
+    };
+
+    this.listSubscriptions = function(callback) {
         callback([]); // We're not able to list all subscriptions
-    },
+    };
 
-	this.isUsing = function(callback) {
-		var that = this;
-		$.get("http://www.typepad.com/dashboard", function(data) {
-			menu = $(data).find(".logged-in-msg")
-			if(menu.length === 0) {
-				callback(false);
-			}
-			else {
-				callback(true);
-			}
-		});
-	}
+    this.isUsing = function(callback) {
+        var that = this;
+        $.get("http://www.typepad.com/dashboard", function(data) {
+            menu = $(data).find(".logged-in-msg");
+            if(menu.length === 0) {
+                callback(false);
+            }
+            else {
+                callback(true);
+            }
+        });
+    };
 
 });
