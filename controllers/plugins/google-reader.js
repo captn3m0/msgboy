685c3cd19ae8d5a16e403cf0508f968d98c5e068
julien <julien.genestoux@gmail.com>
2011-08-06 11:29:33 +0200

    linting google reader

--- controllers/plugins/google-reader.js
+++ controllers/plugins/google-reader.js
@@ -1,56 +1,56 @@
 // Google Reader
-Plugins.register(new function() {
+Plugins.register(function() {
 
-	this.name = 'Google Reader', // Name for this plugin. The user will be asked which plugins he wants to use.
+    this.name = 'Google Reader'; // Name for this plugin. The user will be asked which plugins he wants to use.
 
-	this.onSubscriptionPage = function() {
-		// This method returns true if the plugin needs to be applied on this page.
-		return (window.location.host == "www.google.com" && window.location.pathname == '/reader/view/')
-	},
+    this.onSubscriptionPage = function() {
+        // This method returns true if the plugin needs to be applied on this page.
+        return (window.location.host == "www.google.com" && window.location.pathname == '/reader/view/');
+    };
 
-	this.hijack = function(follow, unfollow) {
-		// This methods hijacks the susbcription action on the specific website for this plugin.
-		var submitted = function() {
-		    follow({
-		        url: $("#quickadd").val(),
-		        title: $("#quickadd").val()
-			}, function() {
-			    // Done
-			});
-		}
-		$("#quick-add-form .goog-button-body").click(submitted)
-		$("#quick-add-form").submit(submitted);
-	},
-		
-	this.listSubscriptions = function(callback) {
-	    links = [];
-		request = new XMLHttpRequest();
-		request.open("GET", "http://www.google.com/reader/subscriptions/export", true);
-		request.onreadystatechange = function() {
-		    var subscriptions = [];
-			if (request.readyState == 4) {
-				urls = $(request.responseXML).find("outline").each(function() {
-					subscriptions.push({
-					    url:  $(this).attr("xmlUrl"),
-					    title:$(this).attr("title")
-					})
-				});
-			}
-			callback(subscriptions);
-		};
-		request.send();
-    },
-	
-	this.isUsing = function(callback) {
-		var that = this;
-		$.get("http://www.google.com/reader/view/", function(data) {
-			if($(data).find(".loginBox").length === 0) {
-				callback(true);
-			}
-			else {
-				callback(false);
-			}
-		});
-	}
-	
+    this.hijack = function(follow, unfollow) {
+        // This methods hijacks the susbcription action on the specific website for this plugin.
+        var submitted = function() {
+            follow({
+                url: $("#quickadd").val(),
+                title: $("#quickadd").val()
+            }, function() {
+                // Done
+            });
+        };
+        $("#quick-add-form .goog-button-body").click(submitted);
+        $("#quick-add-form").submit(submitted);
+    };
+
+    this.listSubscriptions = function(callback) {
+        links = [];
+        request = new XMLHttpRequest();
+        request.open("GET", "http://www.google.com/reader/subscriptions/export", true);
+        request.onreadystatechange = function() {
+            var subscriptions = [];
+            if (request.readyState == 4) {
+                urls = $(request.responseXML).find("outline").each(function() {
+                    subscriptions.push({
+                        url:  $(this).attr("xmlUrl"),
+                        title:$(this).attr("title")
+                    });
+                });
+            }
+            callback(subscriptions);
+        };
+        request.send();
+    };
+
+    this.isUsing = function(callback) {
+        var that = this;
+        $.get("http://www.google.com/reader/view/", function(data) {
+            if($(data).find(".loginBox").length === 0) {
+                callback(true);
+            }
+            else {
+                callback(false);
+            }
+        });
+    };
+
 });
